ae6f70c0ade91cd196876e7ba7fa130730562b23
Szabolcs02 <sza2602@gmail.com>
2024-12-02 19:08:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Fetch random user data from RandomUser.me API
-fetch('https://randomuser.me/api/')
+fetch('https://randomuser.me/api/?gender=male')
   .then(response => response.json())
   .then(data => {
     const user = data.results[0];
 
     // Set profile picture, name, and contact details
     document.getElementById('profile-picture').src = user.picture.large;
-    document.getElementById('name').innerText = `${user.name.first} ${user.name.last}`;
-    document.getElementById('email').innerText = user.email;
-    document.getElementById('phone').innerText = user.phone;
-    document.getElementById('location').innerText = `${user.location.city}, ${user.location.country}`;
+    //document.getElementById('name').innerText = `${user.name.first} ${user.name.last}`;
+    //document.getElementById('email').innerText = user.email;
+    //document.getElementById('phone').innerText = user.phone;
+    //document.getElementById('location').innerText = `${user.location.city}, ${user.location.country}`;
   })
   .catch(error => console.error('Error fetching user data:', error));
